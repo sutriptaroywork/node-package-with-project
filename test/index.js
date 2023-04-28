02d1502902293b1ce2b75a194c1f3cee41dc6697
sutriptaroywork <sutriptaroywork@gmail.com>
2023-04-28 15:34:45 +0530

    calling project

---
+++ test/index.js
@@ -0,0 +1,8 @@
+const { isSutripta, isString } = require('@sutriptaroy/core')
+const core = require('@sutriptaroy/core')
+
+console.log(isSutripta("Sutripta"))
+console.log(isString("Sutripta"))
+
+console.log(core.isSutripta("Sutripta"))
+console.log(core.isString("Sutripta"))
